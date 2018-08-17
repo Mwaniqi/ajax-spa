a9d41ea3ab5b5c36a0fe141c570fbe36c52efbbe
mwaniqi <mwaniqi@gmail.com>
2018-08-17 10:41:33 -0700

    skeleton ajax page loads

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var pageHeader = nav.parentElement
 var navOffset = nav.offsetTop
 var navToggle = document.getElementById('nav-toggle')
 var navLinks = Array.from(nav.children)
+var main = document.getElementById('main')
 
 // .matchMedia to match css media queries
 if (window.matchMedia('(min-width: 37.5rem)').matches) {
@@ -36,3 +37,53 @@ nav.addEventListener('click', function(e) {
   });
   e.target.classList.add('active')
 })
+
+// show home on first load
+document.addEventListener('DOMContentLoaded', function() {
+  loadMain()
+})
+
+function loadMain() {
+  var xhr = new XMLHttpRequest()
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 &&
+        this.status === 200) {
+          console.log(this.responseText)
+          main.innerHTML = this.responseText
+    }
+  }
+
+  xhr.open('GET', '/home.html', true)
+  xhr.send()
+}
+
+function loadTeam() {
+  var xhr = new XMLHttpRequest
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 &&
+        this.status === 200) {
+          console.log(this.responseText)
+          main.innerHTML = this.responseText
+    }
+  }
+
+  xhr.open('GET', '/team.html', true)
+  xhr.send()
+}
+
+function loadProducts() {
+  var xhr = new XMLHttpRequest
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 &&
+        this.status === 200) {
+          console.log(this.responseText)
+          main.innerHTML = this.responseText
+    }
+  }
+
+  xhr.open('GET', '/products.html', true)
+  xhr.send()
+}
