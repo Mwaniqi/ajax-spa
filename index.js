50aa3e4444b12a77a72c613387c08c9abd9a5b5d
mwaniqi <mwaniqi@gmail.com>
2020-06-29 22:58:46 -0700

    added shopping cart markup

--- index.js
+++ index.js
@@ -1,136 +1,142 @@
-var nav = document.getElementById('navbar')
-var pageHeader = nav.parentElement
-var menuIcon = document.querySelector('.icon-menu')
-var navOffset = nav.offsetTop
-var navLinks = Array.from(nav.children)
-var main = document.getElementById('main')
-var photos, users
+var nav = document.querySelector(".nav");
+var pageHeader = nav.parentElement;
+var menuIcon = document.querySelector(".icon-menu");
+var navOffset = nav.offsetTop;
+var navLinks = Array.from(nav.children);
+var main = document.getElementById("main");
+var photos, users;
 
-window.onscroll = () => stickyNav()  
+window.onscroll = () => stickyNav();
 
 // nav collapse
-menuIcon.addEventListener('click', function(e) {
-  nav.classList.toggle('show')
-})
+menuIcon.addEventListener("click", function (e) {
+  nav.classList.toggle("show");
+});
 
 // sticky nav
 function stickyNav() {
   if (window.pageYOffset > navOffset) {
-    nav.classList.add('nav-sticky')
+    nav.classList.add("nav-sticky");
     // pageHeader.style.marginBottom = '2rem'
   } else {
-    nav.classList.remove('nav-sticky')
+    nav.classList.remove("nav-sticky");
     // pageHeader.style.marginBottom = '0'
   }
 }
 
 //  toggle .active class
-nav.addEventListener('click', function(e) {
+nav.addEventListener("click", function (e) {
   navLinks.forEach((link) => {
-    link.classList.remove('active')
+    link.classList.remove("active");
   });
-  e.target.classList.add('active')
-  nav.classList.remove('show')
-})
+  e.target.classList.add("active");
+  nav.classList.remove("showNav");
+});
 
 // show home on first load
-document.addEventListener('DOMContentLoaded', function() {
-  loadHtml('home')
-})
+document.addEventListener("DOMContentLoaded", function () {
+  loadHtml("home");
+});
 
 function loadHtml(page) {
-  var xhr = new XMLHttpRequest()
-  var url = `${page}.html`
+  var xhr = new XMLHttpRequest();
+  var url = `${page}.html`;
 
-  xhr.onload = function() {
+  xhr.onload = function () {
     if (this.readyState === 4 && this.status === 200) {
-      main.innerHTML = this.response
+      main.innerHTML = this.response;
     }
-  }
+  };
 
-  xhr.open('GET', url, true)
-  xhr.send()
+  xhr.open("GET", url, true);
+  xhr.send();
 }
 
 // Promise-based xhr
-var makeRequest = function(url) {
-  var req = new XMLHttpRequest
+var makeRequest = function (url) {
+  var req = new XMLHttpRequest();
 
   // wrap xhr in a Promise
-  return new Promise(function(resolve, reject) {
-    req.onload = function() {
+  return new Promise(function (resolve, reject) {
+    req.onload = function () {
       if (req.status === 200 && req.readyState === 4) {
-        resolve(req)
+        resolve(req);
       } else {
-        reject({statusText: req.statusText})
+        reject({ statusText: req.statusText });
       }
-    }
-    req.open('GET', url, true)
-    req.send()
-  })
-}
+    };
+    req.open("GET", url, true);
+    req.send();
+  });
+};
 
 function buildTeamHtml() {
-  makeRequest('https://jsonplaceholder.typicode.com/photos')
-    .then(function(req) {  
-      photos = JSON.parse(req.response)
-      return makeRequest('https://jsonplaceholder.typicode.com/users')
-    }).then(function(req) {
-      users = JSON.parse(req.response)
-    }).then(function() {
-      main.innerHTML = ''
-      var div = createEl('div')
-      div.classList.add('team')
-      append(main, div)
-
-      users.forEach(function(person) {
-        var ul = createEl('ul')
-        var name = createEl('li')
-        var email = createEl('li')
-        var motto = createEl('li')
-        var phrase = createEl('li')
-        var mottoStr = person.company.bs
-        mottoStr = mottoStr.replace(mottoStr.charAt(0), mottoStr.charAt(0).toUpperCase())
-
-        name.textContent = person.name
-        email.textContent = person.email
-        motto.textContent = mottoStr
-        phrase.textContent = person.company.catchPhrase
-
-        append(div, ul)
-        append(ul, name)
-        append(ul, email)
-        append(ul, motto)
-        append(ul, phrase)
-
-        randomPhoto()
-      })
-    }).catch(function(err) {
-      console.log('Error:', err)
+  makeRequest("https://jsonplaceholder.typicode.com/photos")
+    .then(function (req) {
+      photos = JSON.parse(req.response);
+      return makeRequest("https://jsonplaceholder.typicode.com/users");
     })
+    .then(function (req) {
+      users = JSON.parse(req.response);
+    })
+    .then(function () {
+      main.innerHTML = "";
+      var div = createEl("div");
+      div.classList.add("team");
+      append(main, div);
+
+      users.forEach(function (person) {
+        var ul = createEl("ul");
+        var name = createEl("li");
+        var email = createEl("li");
+        var motto = createEl("li");
+        var phrase = createEl("li");
+        var mottoStr = person.company.bs;
+        mottoStr = mottoStr.replace(
+          mottoStr.charAt(0),
+          mottoStr.charAt(0).toUpperCase()
+        );
+
+        name.textContent = person.name;
+        email.textContent = person.email;
+        motto.textContent = mottoStr;
+        phrase.textContent = person.company.catchPhrase;
+
+        append(div, ul);
+        append(ul, name);
+        append(ul, email);
+        append(ul, motto);
+        append(ul, phrase);
+
+        randomPhoto();
+      });
+    })
+    .catch(function (err) {
+      console.log("Error:", err);
+    });
 }
 
 function createEl(element) {
-  return document.createElement(element)
+  return document.createElement(element);
 }
 
 function append(parent, element) {
-  return parent.appendChild(element)
+  return parent.appendChild(element);
 }
 
 // select random photo
 function randomPhoto() {
-  var index = Math.floor(Math.random() * users.length)
-  var ul = document.querySelectorAll('ul')
-  var photo = photos[index]
-  var img = createEl('img')
-  var li = createEl('li')
+  var index = Math.floor(Math.random() * users.length);
+  var ul = document.querySelectorAll("ul");
+  var photo = photos[index];
+  var img = createEl("img");
+  var li = createEl("li");
 
-  img.src = photo.thumbnailUrl
-  var img_li = append(li, img)
+  img.src = photo.thumbnailUrl;
+  var img_li = append(li, img);
 
   // insert img before other items in ul
-  ul.forEach(function(list) {
-    list.insertBefore(img_li, list.childNodes[0])
-  })
+  ul.forEach(function (list) {
+    list.insertBefore(img_li, list.childNodes[0]);
+  });
 }
