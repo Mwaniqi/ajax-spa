032a5fcd7654b522e2066867bc29d95c67d265d7
mwaniqi <mwaniqi@gmail.com>
2020-07-05 14:53:24 -0700

    add methods to change home page and team page ui using ajax

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-var nav = document.querySelector(".nav");
-var pageHeader = nav.parentElement;
-var menuIcon = document.querySelector(".icon-menu");
-var navOffset = nav.offsetTop;
-var navLinks = Array.from(nav.children);
-var main = document.getElementById("main");
-var photos, users;
+const nav = document.querySelector(".nav");
+const pageHeader = nav.parentElement;
+const menuIcon = document.querySelector(".icon-menu");
+const navOffset = nav.offsetTop;
+const navLinks = Array.from(nav.children);
+let main = document.getElementById("main");
+let productsDOM;
+let teamPhotos, team;
 
 window.onscroll = () => stickyNav();
+
+let App = {
+  init() {
+    let products = new Products();
+    loadHtml("home").then(() => {
+      ui.displayProducts(products);
+    });
+  },
+};
 
 // nav collapse
 menuIcon.addEventListener("click", function (e) {
@@ -31,30 +41,54 @@ nav.addEventListener("click", function (e) {
   });
   e.target.classList.add("active");
   nav.classList.remove("showNav");
-});
-
-// show home on first load
-document.addEventListener("DOMContentLoaded", function () {
-  loadHtml("home");
-});
-
-function loadHtml(page) {
-  var xhr = new XMLHttpRequest();
-  var url = `${page}.html`;
-
-  xhr.onload = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      main.innerHTML = this.response;
+  let pageurl = e.target.innerText.toLowerCase();
+  if (pageurl === "home") {
+    let storedProducts =
+      JSON.parse(localStorage.getItem("products")) || Products();
+    loadHtml("home").then(() => {
+      ui.displayProducts(storedProducts);
+    });
+  }
+  if (pageurl === "team") {
+    let teamData = JSON.parse(localStorage.getItem("teamData"));
+    // getTeamData().then((teamData) => ui.displayTeam(teamData));
+    if (JSON.parse(localStorage.getItem("teamData"))) {
+      ui.displayTeam(teamData);
+    } else {
+      getTeamData().then((data) => ui.displayTeam(data));
     }
-  };
+  }
+  if (pageurl === "contact") {
+    loadHtml("contact");
+  }
+});
 
-  xhr.open("GET", url, true);
-  xhr.send();
+async function loadHtml(page) {
+  try {
+    let result = await fetch(`${page}.html`);
+    let response = await result.text();
+    main.innerHTML = response;
+  } catch (error) {
+    console.error();
+  }
 }
+// function loadHtml(page) {
+//   let xhr = new XMLHttpRequest();
+//   let url = `${page}.html`;
+
+//   xhr.onload = function () {
+//     if (this.readyState === 4 && this.status === 200) {
+//       main.innerHTML = this.response;
+//     }
+//   };
+
+//   xhr.open("GET", url, true);
+//   xhr.send();
+// }
 
 // Promise-based xhr
-var makeRequest = function (url) {
-  var req = new XMLHttpRequest();
+let makeRequest = function (url) {
+  let req = new XMLHttpRequest();
 
   // wrap xhr in a Promise
   return new Promise(function (resolve, reject) {
@@ -70,50 +104,109 @@ var makeRequest = function (url) {
   });
 };
 
-function buildTeamHtml() {
-  makeRequest("https://jsonplaceholder.typicode.com/photos")
-    .then(function (req) {
-      photos = JSON.parse(req.response);
-      return makeRequest("https://jsonplaceholder.typicode.com/users");
-    })
-    .then(function (req) {
-      users = JSON.parse(req.response);
-    })
-    .then(function () {
-      main.innerHTML = "";
-      var div = createEl("div");
-      div.classList.add("team");
-      append(main, div);
-
-      users.forEach(function (person) {
-        var ul = createEl("ul");
-        var name = createEl("li");
-        var email = createEl("li");
-        var motto = createEl("li");
-        var phrase = createEl("li");
-        var mottoStr = person.company.bs;
-        mottoStr = mottoStr.replace(
-          mottoStr.charAt(0),
-          mottoStr.charAt(0).toUpperCase()
-        );
-
-        name.textContent = person.name;
-        email.textContent = person.email;
-        motto.textContent = mottoStr;
-        phrase.textContent = person.company.catchPhrase;
-
-        append(div, ul);
-        append(ul, name);
-        append(ul, email);
-        append(ul, motto);
-        append(ul, phrase);
-
-        randomPhoto();
-      });
-    })
-    .catch(function (err) {
-      console.log("Error:", err);
+let ui = {
+  displayProducts(products) {
+    productsDOM = document.querySelector(".products");
+    let result = "";
+    products.forEach((product) => {
+      result += `
+        <!-- single product -->
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fa fa-shopping-cart">Add to cart</i>
+            </button>
+          </div>
+          <h3>${product.name}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+        <!-- end single product -->`;
     });
+    productsDOM.innerHTML = result;
+  },
+  displayTeam(teamData) {
+    main.innerHTML = "";
+    let div = createEl("div");
+    div.classList.add("team");
+    append(main, div);
+    let list = "";
+    console.l;
+    teamData.team.forEach(function (person) {
+      list += `
+        <ul>
+          <img src=${randomPhotoUrl(teamData)}>
+          <li>${person.name}</li>
+          <li>${person.email}</li>
+          <li>${person.company.bs}</li>
+          <li>${person.company.catchPhrase}</li>
+        </ul>`;
+      div.innerHTML = list;
+    });
+  },
+};
+
+let Store = {
+  saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  },
+  saveTeamData(teamData) {
+    localStorage.setItem("teamData", JSON.stringify(teamData));
+  },
+};
+
+// get products
+function Products() {
+  let products = [];
+  for (let i = 0; i < 10; i++) {
+    const name = faker.commerce.productName();
+    const price = faker.commerce.price();
+    const image = faker.image.technics();
+    const id = faker.random.uuid();
+    products.push({ name, price, image, id });
+  }
+  Store.saveProducts(products);
+  return products;
+}
+
+// get team data
+// function getTeamData() {
+//   makeRequest("https://jsonplaceholder.typicode.com/albums/1/photos")
+//     .then(function (req) {
+//       teamPhotos = JSON.parse(req.response);
+//       return makeRequest("https://jsonplaceholder.typicode.com/users");
+//     })
+//     .then(function (req) {
+//       team = JSON.parse(req.response);
+//     })
+//     .then(function () {
+//       let teamData = { teamPhotos, team };
+//       // Store.saveTeamData(teamData);
+//       return teamData;
+//     })
+//     .catch(function (err) {
+//       console.log("Error:", err);
+//     });
+// }
+
+async function getTeamData() {
+  try {
+    let teamPhotos = await fetch(
+      "https://jsonplaceholder.typicode.com/albums/1/photos"
+    );
+    let team = await fetch("https://jsonplaceholder.typicode.com/users");
+    teamPhotos = await teamPhotos.json();
+    team = await team.json();
+    let teamData = { teamPhotos, team };
+    Store.saveTeamData(teamData);
+    return teamData;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function createEl(element) {
@@ -125,18 +218,12 @@ function append(parent, element) {
 }
 
 // select random photo
-function randomPhoto() {
-  var index = Math.floor(Math.random() * users.length);
-  var ul = document.querySelectorAll("ul");
-  var photo = photos[index];
-  var img = createEl("img");
-  var li = createEl("li");
-
-  img.src = photo.thumbnailUrl;
-  var img_li = append(li, img);
-
-  // insert img before other items in ul
-  ul.forEach(function (list) {
-    list.insertBefore(img_li, list.childNodes[0]);
-  });
+function randomPhotoUrl(data) {
+  let index = Math.floor(Math.random() * data.team.length);
+  let photo = data.teamPhotos[index];
+  return photo.thumbnailUrl;
 }
+// show home on first load
+document.addEventListener("DOMContentLoaded", function () {
+  App.init();
+});
